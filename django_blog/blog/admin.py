15c6041cca2bae49f555541c240e99f1f6274f83
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-10-02 11:32:32 +0200

    add models to admin panel

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post, Comment, Like
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    list_filter = ('created_date', 'published_date')
+    search_fields = ('title', 'content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_date', 'published_date')
+    list_filter = ('approved_comment', 'created_date')
+    search_fields = ('post', 'author')
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'created_date')
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -20,5 +20,20 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='commets')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+    
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together = ('post', 'user')
